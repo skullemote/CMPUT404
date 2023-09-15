09723c49f6b505cdebcb038ebc4320a9ea996cc1
skullemote <thakursparsh28@gmail.com>
2023-09-15 13:36:30 -0600

    lab 1 final

--- lab01/lab1.py
+++ lab01/lab1.py
@@ -1,4 +1,4 @@
 import requests
 #print(requests.__version__)
-resp = requests.get("http://google.com/")
+resp = requests.get("https://raw.githubusercontent.com/skullemote/CMPUT404/main/lab01/lab1.py")
 print(resp.text)
