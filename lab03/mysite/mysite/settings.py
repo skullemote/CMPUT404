b0baef67492e255420503f277ab0555b4b0e07f1
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-10-25 15:00:06 -0600

    heroku 3

--- lab03/mysite/mysite/settings.py
+++ lab03/mysite/mysite/settings.py
@@ -1,3 +1,5 @@
+import os 
+import django_on_heroku
 """
 Django settings for mysite project.
 
@@ -12,7 +14,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
